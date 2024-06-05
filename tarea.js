d530396dbcaaae67437eb62dfb6bb840d9f48b64
Juan Loretto <juanloretto29@gmail.com>
2024-06-05 16:42:00 -0300

    tarea de la clase

---
+++ tarea.js
@@ -0,0 +1,22 @@
+/* 1 */
+let apellido = 'Perez'
+
+prompt('apellido')
+
+/* 2 */
+console.log(`El apellido tiene ${apellido.length} caracteres`)
+
+/* 3 */
+console.log(apellido.at(-1))
+
+/* Tarea 2*/
+
+let nombre='marmota'
+
+console.log(nombre.toUpperCase())
+
+/* 2 */
+
+let nombreCapitalizado=(nombre.at(0).toUpperCase())
+let restoNombre=(nombre.substring(1))
+console.log(`${nombreCapitalizado}${restoNombre}`)
