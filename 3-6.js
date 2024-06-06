4808546dcbef6c5732e8c591a99c1ffd7f7e529b
Juan Loretto <juanloretto29@gmail.com>
2024-06-06 12:26:48 -0300

    agrego tarea del 3-6-24

---
+++ 3-6.js
@@ -0,0 +1,18 @@
+//Tarea
+// Crear dos variables de tipo number
+let numero1 = 56
+let numero2 = 24
+// mostrar por consola los resultados de las siguientes operaciones:
+
+// Sumar las dos variables
+console.log(numero1+numero2)
+// Restar las dos variables
+console.log(numero1-numero2)
+// Multiplicar las dos variables
+console.log(numero1*numero2)
+// Dividir ambas variables
+console.log(numero1/numero2)
+// comparar si la primera variable es mayor o igual que la segunda
+console.log(numero1>=numero2)
+// Comparar si la primera variable es distinta de la segunda
+console.log(numero1==numero2)
